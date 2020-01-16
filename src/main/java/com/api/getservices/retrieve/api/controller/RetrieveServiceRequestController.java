ad8ea27c7b567526d8010b8592669d48b690118e
kishorepatha <kishorepatha@kishores-mbp.cgocable.net>
2020-01-15 23:55:48 -0500

    .

--- src/main/java/com/api/getservices/retrieve/api/controller/RetrieveServiceRequestController.java
+++ src/main/java/com/api/getservices/retrieve/api/controller/RetrieveServiceRequestController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.getservices.retrieve.api.domain.RetrieveServiceResponse;
@@ -22,13 +23,13 @@ public class RetrieveServiceRequestController {
 
 	@GetMapping(value = "/retrieveRequests/request/{requestId}")
 	public ResponseEntity<RetrieveServiceResponse> retrieve(
-			@PathVariable String requestId) {
+			@PathVariable String requestId, @RequestHeader("authorizationCode") String authorizationCode) {
 		return ResponseEntity.ok(retrieveRequestService
 				.retrieveRequest(requestId));
 	}
 	
 	@GetMapping(value = "/retrieveRequests")
-	public ResponseEntity<List<RetrieveServiceResponse>> findAll() {
+	public ResponseEntity<List<RetrieveServiceResponse>> findAll(@RequestHeader("authorizationCode") String authorizationCode) {
 		return ResponseEntity.ok(retrieveRequestService
 				.findAllRequests());
 	}
